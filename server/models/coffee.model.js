e4aca72d7bfac356859cb90427e842f33a822297
Kenneth Schaefer <kenneth.schaefer0@gmail.com>
2022-08-13 09:24:22 -0500

    5 passing, 50 pending

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,45 @@
 const Sequelize = require('sequelize')
 const db = require('./database')
+const Op=Sequelize.Op;
 
 const Coffee = db.define('coffee', {
-  // your code here
+  name:{
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  ingredients: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
 })
+
+Coffee.prototype.getIngredients = function () {
+
+  return this.ingredients.join(', ');
+}
+
+Coffee.findByIngredient = (ingredient) =>{
+  return Coffee.findAll({
+    where: {
+      ingredients: {
+        [Op.contains]: [ingredient]
+      }
+    }
+  })
+}
+
+Coffee.beforeValidate(validate);
+
+function validate(coffee){
+  if(!coffee.ingredients.includes('love')){
+    coffee.ingredients.push('love');
+  }
+  }
 
 module.exports = Coffee
